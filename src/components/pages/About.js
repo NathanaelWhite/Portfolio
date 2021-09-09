08f1ed4f322d7a060684e0e78f57a5f0bb8fed73
Nathanael White <narhanaelw99@gmail.com>
2021-09-08 21:05:09 -0500

    edited font size in about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,18 +1,20 @@
-import React from 'react'
-
+import React from "react";
 
 function About() {
-
   return (
-    <section className="my-5" >
+    <section className="my-5">
       <h1 id="about">About Me</h1>
       <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-      </p>
+        <p className="about">
+          I graduated from the UT Austin Coding Bootcamp in September of 2021.
+          I've coached a multitude of fitness methods since 2014 (gymnastics,
+          CrossFit, strength & conditioning). I worked as a therapy practitioner
+          with Synergy Release Method from 2016-2021 when I found a passion for
+          web development.
+        </p>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
